8b804669811c0684799ccc63b185bd6803c076cf
Maxim <askimarhon@mail.ru>
2019-07-22 14:18:49 +0300

    start commit

---
+++ src/types.h
@@ -0,0 +1,10 @@
+#ifndef TYPES_H_
+#define TYPES_H_
+
+#include <vector>
+#include <array>
+
+using ip_pool_int= std::vector<std::array<int,4>>;
+using ip_pool_string = std::vector<std::string>;
+
+#endif /* TYPES_H_ */
